4a1250890631d0f03275bbe2025f9e35895e502c
hpy hs <hshpy.pengyu@gmail.com>
2025-08-27 16:11:58 +0800

    add ignore direct link args

--- server/handles/down.go
+++ server/handles/down.go
@@ -114,7 +114,7 @@ func proxy(c *gin.Context, link *model.Link, file model.Obj, proxyRange bool) {
 		link = common.ProxyRange(c, link, file.GetSize())
 	}
 	Writer := &common.WrittenResponseWriter{ResponseWriter: c.Writer}
-	raw, _ := strconv.ParseBool(c.Query("raw"))
+	raw, _ := strconv.ParseBool(c.DefaultQuery("raw", "false"))
 	if utils.Ext(file.GetName()) == "md" && setting.GetBool(conf.FilterReadMeScripts) && !raw {
 		buf := bytes.NewBuffer(make([]byte, 0, file.GetSize()))
 		w := &common.InterceptResponseWriter{ResponseWriter: Writer, Writer: buf}
