f94b2639299ac6af67977e86e65f1346cc17435f
j2rong4cn <j2rong@qq.com>
2025-12-02 22:49:14 +0800

    添加 GetRootPath 方法到多个驱动

--- drivers/alias/driver.go
+++ drivers/alias/driver.go
@@ -73,14 +73,11 @@ func (d *Alias) Drop(ctx context.Context) error {
 	return nil
 }
 
+func (Addition) GetRootPath() string {
+	return "/"
+}
+
 func (d *Alias) Get(ctx context.Context, path string) (model.Obj, error) {
-	if utils.PathEqual(path, "/") {
-		return &model.Object{
-			Name:     "Root",
-			IsFolder: true,
-			Path:     "/",
-		}, nil
-	}
 	root, sub := d.getRootAndPath(path)
 	dsts, ok := d.pathMap[root]
 	if !ok {
