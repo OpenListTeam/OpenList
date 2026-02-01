fd6fa38ce05b0cd73043df559f74a730035f6cd6
Shelton Zhu <498220739@qq.com>
2026-02-02 01:18:20 +0800

    feat: add thumbnail support for 115 driver and 115 share
    - Add Thumb() method to FileObj in 115 driver to return thumbnail URL
    - Add ThumbURL field to FileObj struct in 115 share utility
    - Update 115driver dependency from v1.2.2 to v1.2.3 to support thumbnail functionality
    - Implement Thumb() method for 115 share FileObj to return thumbnail URL

--- drivers/115_share/utils.go
+++ drivers/115_share/utils.go
@@ -21,6 +21,7 @@ type FileObj struct {
 	FileName string
 	isDir    bool
 	FileID   string
+	ThumbURL string
 }
 
 func (f *FileObj) CreateTime() time.Time {
@@ -55,6 +56,10 @@ func (f *FileObj) GetPath() string {
 	return ""
 }
 
+func (f *FileObj) Thumb() string {
+	return f.ThumbURL
+}
+
 func transFunc(sf driver115.ShareFile) (model.Obj, error) {
 	timeInt, err := strconv.ParseInt(sf.UpdateTime, 10, 64)
 	if err != nil {
@@ -75,6 +80,7 @@ func transFunc(sf driver115.ShareFile) (model.Obj, error) {
 		FileName: string(sf.FileName),
 		isDir:    isDir,
 		FileID:   fileID,
+		ThumbURL: sf.ThumbURL,
 	}, nil
 }
 
