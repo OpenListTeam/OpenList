81f0c2d9df3f4175f09845abd5689a61b1e9da8e
mkitsdts <mkitsdts@outlook.com>
2026-03-26 11:15:57 +0800

    chore(middlewares): move middlewares from server to pkg

--- server/debug.go
+++ server/debug.go
@@ -7,8 +7,8 @@ import (
 
 	"github.com/OpenListTeam/OpenList/v4/internal/conf"
 	"github.com/OpenListTeam/OpenList/v4/internal/sign"
+	"github.com/OpenListTeam/OpenList/v4/pkg/middlewares"
 	"github.com/OpenListTeam/OpenList/v4/server/common"
-	"github.com/OpenListTeam/OpenList/v4/server/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
