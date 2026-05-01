569dedfbce0d7b6dd63bdb4025a5fa0f6e289157
ZZ0YY <2111479855@qq.com>
2026-05-01 17:54:06 +0800

    fix:go fmt

--- drivers/cloudflare_imgbed/meta.go
+++ drivers/cloudflare_imgbed/meta.go
@@ -1,32 +1,32 @@
 package cloudflare_imgbed
 
 import (
-        "github.com/OpenListTeam/OpenList/v4/internal/driver"
-        "github.com/OpenListTeam/OpenList/v4/internal/op"
+	"github.com/OpenListTeam/OpenList/v4/internal/driver"
+	"github.com/OpenListTeam/OpenList/v4/internal/op"
 )
 
 type Addition struct {
-        driver.RootPath
-        Address string `json:"address" type:"text" required:"true" default:"" help:"API domain, https://img.example.com"`
-        Token   string `json:"token" type:"text" required:"true" default:"" help:"API authentication token"`
+	driver.RootPath
+	Address string `json:"address" type:"text" required:"true" default:"" help:"API domain, https://img.example.com"`
+	Token   string `json:"token" type:"text" required:"true" default:"" help:"API authentication token"`
 }
 
 var config = driver.Config{
-        Name:              "cloudflare_imgbed",
-        LocalSort:         false,
-        OnlyProxy:         false,
-        NoCache:           false,
-        NoUpload:          true,
-        NeedMs:            false,
-        DefaultRoot:       "/",
-        CheckStatus:       false,
-        Alert:             "",
-        NoOverwriteUpload: false,
-        NoLinkURL:         false,
+	Name:              "cloudflare_imgbed",
+	LocalSort:         false,
+	OnlyProxy:         false,
+	NoCache:           false,
+	NoUpload:          true,
+	NeedMs:            false,
+	DefaultRoot:       "/",
+	CheckStatus:       false,
+	Alert:             "",
+	NoOverwriteUpload: false,
+	NoLinkURL:         false,
 }
 
 func init() {
-        op.RegisterDriver(func() driver.Driver {
-                return &CFImgBed{}
-        })
+	op.RegisterDriver(func() driver.Driver {
+		return &CFImgBed{}
+	})
 }
