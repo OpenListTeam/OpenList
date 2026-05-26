379958b5efc5b57d7f97d432c7ab6b3d2169b581
pikachuim <pikachuim@qq.com>
2026-05-26 12:37:57 +0800

    fix(vhost): remove useless files

--- server/middlewares/virtual_host.go
+++
@@ -1,4 +0,0 @@
-package middlewares
-
-// Note: Virtual host resolution is handled by existing handlers/middlewares.
-// This file intentionally contains no additional code to avoid unused/dead middleware.
